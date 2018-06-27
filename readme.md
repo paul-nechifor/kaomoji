cef0bd4c5cc56f44d5a347893d9bef3eeac4c15f
Paul Nechifor <paul@nechifor.net>
2018-06-27 20:39:57 +0100

    Move to GitLab.

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
+# This repository has been moved to [gitlab.com/paul-nechifor/kaomoji](http://gitlab.com/paul-nechifor/kaomoji).
+
+Old readme:
+
 # Kaomoji
 
 ## TODO
--- src/app/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Main from './Main';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import {render} from 'react-dom';
-
-// Needed for onTouchTap (http://stackoverflow.com/a/34015469/988941).
-injectTapEventPlugin();
-
-render(<Main/>, document.getElementById('app'));
